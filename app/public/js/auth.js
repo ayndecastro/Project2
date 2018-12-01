87094aa9d451fdec96817815fd6413ee245c14d4
ayn <vincentayndecastro@gmail.com>
2018-12-01 09:54:12 -0600

    fixed sign up

--- app/public/js/auth.js
+++ app/public/js/auth.js
@@ -3,11 +3,11 @@ console.log("auth");
 jQuery(document).ready(function() {
     console.log("jquery");
 
-    if(window.location === "/signin"){
-        $(".signup").on("click", function(){
-          window.location = "https://project2present.herokuapp.com/signup";
-        })
-    } 
+    // if(window.location === "/signin"){
+    //     $(".signup").on("click", function(){
+    //       window.location = "https://project2present.herokuapp.com/signup";
+    //     })
+    // } 
     // if(window.location === "/signin"){
     //     $(".login").on("click", login)
     // }
